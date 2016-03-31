33b4a0e436b3d1c2a4f03f18d3389304c4770545
Gaston Lenotre <gastonlenotre@gmail.com>
2016-03-31 13:32:02 -0500

    added a .ignore, added js/jquery bootstrap in index

--- views/partials/navbar.php
+++ views/partials/navbar.php
@@ -3,7 +3,7 @@
   <div class="container-fluid">
 	<!-- Brand and toggle get grouped for better mobile display -->
     <div class="navbar-header">
-      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-haspopup="true" aria-expanded="true">
         <span class="sr-only">Toggle navigation</span>
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
@@ -17,27 +17,28 @@
       <ul class="nav navbar-nav">
         <li class="active"><a href="#">About <span class="sr-only">(current)</span></a></li>
         <li class="dropdown">
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Food <span class="caret"></span></a>
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="true">Food <span class="caret"></span></a>
           <ul class="dropdown-menu">
-            <li><a href="#">Action</a></li>
-            <li><a href="#">Another action</a></li>
-            <li><a href="#">Something else here</a></li>
-            <li role="separator" class="divider"></li>
+            <li><a href="#">Grapes</a></li>
+            <li><a href="#">Raisins</a></li>
+            <li><a href="#">Jelly</a></li>
+            <li><a href="#">Vinegar</a></li>
+<!--             <li role="separator" class="divider"></li>
             <li><a href="#">Separated link</a></li>
             <li role="separator" class="divider"></li>
-            <li><a href="#">One more separated link</a></li>
+            <li><a href="#">One more separated link</a></li> -->
           </ul>
         </li>
         <li class="dropdown">
           <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Drink <span class="caret"></span></a>
           <ul class="dropdown-menu">
-            <li><a href="#">Action</a></li>
-            <li><a href="#">Another action</a></li>
-            <li><a href="#">Something else here</a></li>
+            <li><a href="#">Red wines</a></li>
+            <li><a href="#">White wines</a></li>
+            <li><a href="#">Grape juice</a></li><!-- 
             <li role="separator" class="divider"></li>
             <li><a href="#">Separated link</a></li>
             <li role="separator" class="divider"></li>
-            <li><a href="#">One more link</a></li>
+            <li><a href="#">One more link</a></li> -->
           </ul>
         </li>
         <li><a href="#">Events</a></li>
