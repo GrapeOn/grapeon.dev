343f113bd46d376d1b1271b106cd54a0fac1f186
Margot McMahon <amcmahon@nes.ru>
2016-04-01 12:02:27 -0500

    bootrap fixed

--- bootstrap.php
+++ bootstrap.php
@@ -5,6 +5,6 @@ require_once('utils/Input.php');
 require_once('utils/Auth.php');
 require_once('utils/Logger.php');
 //models
-require_once('models/Ad.php');
 require_once('models/BaseModel.php');
+require_once('models/Ad.php');
 require_once('models/User.php');
