4e43290d7134676c1282a28dc34e0ab5303cb550
Gaston Lenotre <gastonlenotre@gmail.com>
2016-04-01 13:52:46 -0500

    changed food and drink tabs, added <br> under ads.index

--- views/partials/navbar.php
+++ views/partials/navbar.php
@@ -21,7 +21,6 @@
             <li><a href="#">Grapes</a></li>
             <li><a href="#">Raisins</a></li>
             <li><a href="#">Jelly</a></li>
-            <li><a href="#">Vinegar</a></li>
 <!--             <li role="separator" class="divider"></li>
             <li><a href="#">Separated link</a></li>
             <li role="separator" class="divider"></li>
@@ -31,9 +30,8 @@
         <li class="dropdown">
           <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Drink <span class="caret"></span></a>
           <ul class="dropdown-menu">
-            <li><a href="#">Red Wines</a></li>
-            <li><a href="#">White Wines</a></li>
-            <li><a href="#">Grape Juice</a></li><!-- 
+            <li><a href="#">Wines</a></li>
+            <li><a href="#">Juice</a></li><!-- 
             <li role="separator" class="divider"></li>
             <li><a href="#">Separated link</a></li>
             <li role="separator" class="divider"></li>
