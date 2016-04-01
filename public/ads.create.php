d3cba053445fc3593af92411f62bc6fc1ceb011b
Margot McMahon <amcmahon@nes.ru>
2016-04-01 10:54:08 -0500

    Input class added, required in bootstrap.php

--- public/ads.create.php
+++ public/ads.create.php
@@ -2,6 +2,27 @@
 ?>
 <?php
     require '../bootstrap.php';
+   //conditional statement binding user input to :variables, then INSERTING them INTO ad_table
+   if (
+	(Input::has('discount_nameSubmission'))
+	&& (Input::getString('discount_nameSubmission') != "")
+	&& (Input::has('descriptionSubmission'))
+	&& (Input::getNumber('descriptionSubmission') != "")
+	&& (Input::getString('percent_offSubmission'))
+	&& (Input::has('percent_offSubmission') != "")
+	&& (Input::getDate('start_dateSubmission'))
+	&& (Input::has('start_dateSubmission') != "")
+	&& (Input::getString('end_dateSubmission'))
+	&& (Input::has('end_dateSubmission') != "")
+	&& (Input::getString('business_nameSubmission'))
+	&& (Input::has('business_nameSubmission') != "")
+	&& (Input::getString('business_addressSubmission'))
+	&& (Input::has('business_addressSubmission') != "")
+	&& (Input::getString('zip_codeSubmission'))
+	&& (Input::has('zip_codeSubmission') != "")
+	&& (Input::getString('imgSubmission'))
+	&& (Input::has('imgSubmission') != "")
+	) {};
 ?>
 
 <!DOCTYPE html>
@@ -24,30 +45,30 @@
 	<?php require_once '../views/partials/footer.php'; ?>
 
 <form method="POST" action="national_parks.php">
-	<label for="discount_name">Discount name</label>
-	<input type="text" name="discount_name" id="discount_name">
+	<label for="discount_nameSubmission">Discount name</label>
+	<input type="text" name="discount_nameSubmission" id="discount_nameSubmission">
 	<br><br>
-	<label for="description">A quick description!</label>
-	<input type="text" name="description" id="description">
+	<label for="descriptionSubmission">A quick description!</label>
+	<input type="text" name="descriptionSubmission" id="descriptionSubmission">
 	<br><br>
-	<label for="percent_off">Percent off</label>
-	<input type="text" name="percent_off" id="percent_off">
+	<label for="percent_offSubmission">Percent off</label>
+	<input type="text" name="percent_offSubmission" id="percent_offSubmission">
 	<br><br>
-	<label for="start_date">Start date (YYYY-MM-DD)</label>
-	<input type="text" name="start_date" id="start_date">
+	<label for="start_dateSubmission">Start date (YYYY-MM-DD)</label>
+	<input type="text" name="start_dateSubmission" id="start_dateSubmission">
 	<br><br>
-	<label for="end_date">End date</label>
-	<input type="text" name="end_date" id="end_date">
+	<label for="end_dateSubmission">End date</label>
+	<input type="text" name="end_dateSubmission" id="end_dateSubmission">
 	<br><br>
 	<!--NOTE: date_added will be added to the user submission through a PHP timestamp YYYY-MM-DD -->
-	<label for="business_name">Business or location name</label>
-	<input type="text" name="business_name" id="business_name">
+	<label for="business_nameSubmission">Business or location name</label>
+	<input type="text" name="business_nameSubmission" id="business_nameSubmission">
 	<br><br>
-	<label for="business_address">Street address</label>
-	<input type="text" name="business_address" id="business_address">
+	<label for="business_addressSubmission">Street address</label>
+	<input type="text" name="business_addressSubmission" id="business_addressSubmission">
 	<br><br>
-	<label for="zip_code">Zip code</label>
-	<input type="text" name="zip_code" id="zip_code">
+	<label for="zip_codeSubmission">Zip code</label>
+	<input type="text" name="zip_codeSubmission" id="zip_codeSubmission">
 	<br><br>
 	<h1>IMAGE UPLOADER GOES HERE!</h1>
 	<button type="submit">Grape Job!</button>
