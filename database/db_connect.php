4330c7e874148dfb359b85306bc3397fb68f90c0
Gaston Lenotre <gastonlenotre@gmail.com>
2016-04-06 10:55:15 -0500

    added .env file in .ignore, .env includes an array of sensitive  DB_ info, bootstrap.php includes .env through , db_connect calls

--- database/db_connect.php
+++ database/db_connect.php
@@ -2,10 +2,10 @@
 
 
 //constants defined
-define('DB_NAME', 'grapes_db');
-define('DB_USER', 'grape');
-define('DB_PASSWORD', 'grape');
-define('DB_HOST', '127.0.0.1');
+define('DB_NAME', $_ENV['DB_NAME']);
+define('DB_USER', $_ENV['DB_USER']);
+define('DB_PASSWORD', $_ENV['DB_PASS']);
+define('DB_HOST', $_ENV['DB_HOST']);
 
 //create database connection
 $dbc = new PDO(
